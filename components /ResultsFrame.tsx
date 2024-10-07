cdca7c8baf1cab6d8024313486d555ef831c3de0
jiaoyinyang <vangbe@protonmail.com>
2024-10-07 16:43:10 -0500

    renew

---
+++ components /ResultsFrame.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './../styles/styles.css';
+
+const ResultsFrame: React.FC = () => {
+  return (
+    <div className="large-frame">
+      <h5>Available Cars -</h5>
+      <p>List of cars will be displayed here.</p>
+      {/* Placeholder for car listings */}
+    </div>
+  );
+};
+
+export default ResultsFrame;
