40ac320b820f3e169a9d40028476b3bd9fa6e430
Brandi R <167109599+Blushiva@users.noreply.github.com>
2024-10-07 12:11:26 -0500

    added notations

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,11 @@
-import { Outlet } from "react-router-dom"
+// Must install pkgs to keep red line from yelling at us 
+//npm install react-router-dom
+//npm install @types/react-router-dom
+//npm install @types/react
+import { Outlet } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
-import Navbar from "./components/Navbar"
-
+// pulls from main.tsx?
 function App() {
   return (
     <>
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,19 @@
+// Code for rendering the app and routing
+// must install required packages
+// npm install react-router-dom
+// npm install @types/react-router-dom
+// npm install @types/react
+// must make sure tsconfig is configured correctly
+// created to handle client side routing
+//also imports CSS files and React components that are used as pages
 import ReactDOM from 'react-dom/client'
+
+// this function serves as main function and this also handles routing errors
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+// this is the default route renders the main page component
 import './index.css'
 
+//other routes are defined in the children array they include paths like VolunteerPage, VolunteerForm, EditVolunteer, EditWork
 import App from './App.tsx';
 import ErrorPage from './pages/ErrorPage.tsx';
 import MainPage from './pages/MainPage.tsx';
@@ -10,7 +22,8 @@ import VolunteerForm from './pages/VolunteerForm.tsx';
 import EditVolunteer from './pages/EditVolunteer.tsx';
 import EditWork from './pages/EditWork.tsx';
 
-
+// this passes the router to the router provider as a prop
+// the app uses defined routes to render the components based on current url
 const router = createBrowserRouter([
   {
     path: '/',
