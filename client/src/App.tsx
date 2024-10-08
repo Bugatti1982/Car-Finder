f5f37a3704cccd3f52ddbaa0c0f5cc6d150787c1
Brandi R <167109599+Blushiva@users.noreply.github.com>
2024-10-07 21:24:07 -0500

    added notation to explain the parts

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,10 +2,15 @@
 //npm install react-router-dom
 //npm install @types/react-router-dom
 //npm install @types/react
+// this renders the matched child route's element
 import { Outlet } from "react-router-dom";
+//imported from the components directory
 import Navbar from "./components/Navbar";
 
-// pulls from main.tsx?
+// pulls from main.tsx
+// this is the main component that renders the Navbar and the Outlet
+// the outlet renders the matched child route's element
+// This allows for nested routing; the app component can serve as a layout for different pages
 function App() {
   return (
     <>
