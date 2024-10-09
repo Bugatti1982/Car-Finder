ad157ae20e8db4dc8b68ea0e8ec447bf0f8dd27a
Brandi R <167109599+Blushiva@users.noreply.github.com>
2024-10-09 09:33:10 -0500

    added notation for react implementation

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,5 +1,13 @@
+// Imports from the DOM library
+// This link changes the URL without causeing a page refresh
+// dependant on the npm package react-router-dom
+// import the link component from the react-router-dom package and REACT
+// ********WE NEED TO take slashes off the line below when we finish the code********
+
+// import React from 'react';
 import { Link } from "react-router-dom";
 
+// this renders the Nav bar on the page and links to home page
 const Navbar = () => {
   return (
     <div className="nav">
