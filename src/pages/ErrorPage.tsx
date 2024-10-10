9dc891f03bcc96699ca4eec3c2682ff0a54153da
0xnullLight <vangbe@protonmail.com>
2024-10-10 17:24:40 -0500

    "fix"

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,19 +1,3 @@
-<<<<<<< HEAD:src/pages/ErrorPage.tsx
-// src/pages/ErrorPage.tsx
-import React from 'react';
-import { Box, Typography } from '@mui/material';
-
-const ErrorPage: React.FC = () => {
-    return (
-        <Box sx={{ padding: 4 }}>
-            <Typography variant="h4">Error 404</Typography>
-            <Typography variant="body1">Page not found!</Typography>
-        </Box>
-    );
-};
-
-export default ErrorPage;
-=======
 // throws out an error page if page not found
 const ErrorPage = () => {
   return (
@@ -25,4 +9,3 @@ const ErrorPage = () => {
 };
 
 export default ErrorPage;
->>>>>>> f0fb3fde482e6a9b817507767038f52966805dcc:client/src/pages/ErrorPage.tsx
