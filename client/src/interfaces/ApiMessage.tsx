f33abb883af4aa3ac7b4027a8bd2661b8ae49036
0xnullLight <vangbe@protonmail.com>
2024-10-17 08:13:12 -0500

    delete

--- client/src/interfaces/ApiMessage.tsx
+++
@@ -1,5 +0,0 @@
-// Typescript interface for the ApiMessage  the obiect is the message from the API
-// used to typecheck objects that are returned from the API and turns it to a string
-export interface ApiMessage {
-  message: string;
-}
